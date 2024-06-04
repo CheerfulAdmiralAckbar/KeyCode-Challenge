b15f7836f4fc3b1970096a13f41fd923d4ec8a7e
Ben Johnston <admiralackbar411@gmail.com>
2024-06-04 15:10:05 +0100

    Added JavaScript file and got initial working keycode generator

---
+++ script.js
@@ -0,0 +1,21 @@
+const eventKey = document.getElementById('event-key');
+const eventLocation = document.getElementById('event-location');
+const eventCode = document.getElementById('event-code');
+const eventWhich = document.getElementById('code-which');
+
+const keyCodeMessageText = document.getElementsByClassName('keycode-message-text')[0];
+const whichText = document.getElementById('info-text');
+
+document.addEventListener('keypress', (event) => {
+  console.log(event.key);
+  console.log(event.which);
+  console.log(event.code)
+  eventKey.textContent = event.key;
+  eventLocation.textContent = event.location;
+  eventCode.textContent = event.code;
+
+  keyCodeMessageText.style.display = 'none';
+  eventWhich.style.display = 'block';
+  eventWhich.textContent = event.which;
+  whichText.style.display = 'block';
+});
