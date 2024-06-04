c9e5d9ae63a03284f9e963597027189ff564a3cb
Ben Johnston <admiralackbar411@gmail.com>
2024-06-04 15:25:01 +0100

    Added switch statement to render out strings based on event.location and changed to keydown to pickup locations other than 1 and 3

--- script.js
+++ script.js
@@ -7,13 +7,31 @@ const keyCodeMessageText = document.getElementsByClassName('keycode-message-text
 const whichText = document.getElementById('info-text');
 const keyCodeHeader = document.getElementById('keycode-header-text');
 
-document.addEventListener('keypress', (event) => {
+document.addEventListener('keydown', (event) => {
   console.log(event.key);
   console.log(event.which);
-  console.log(event.code)
+  console.log(event.code);
   eventKey.textContent = event.key;
   eventLocation.textContent = event.location;
   eventCode.textContent = event.code;
+
+  switch (event.location) {
+    case 0:
+      eventLocation.textContent = 'General Keys'
+      break;
+    case 1:
+      eventLocation.textContent = 'Left (Modifier Keys)'
+      break;
+    case 2:
+      eventLocation.textContent = 'Right (Modifier Keys)'
+      break;
+    case 3:
+      eventLocation.textContent = 'Numpad Keys';
+      break;
+    default:
+      eventLocation.textContent = 'Unknown'
+      break;
+  }
 
   keyCodeMessageText.style.display = 'none';
   eventWhich.style.display = 'block';
